54daadb997595e855349a09a52d0e2d88dd249bb
Ryan Tanay <ryantanay@newevolution.org>
2015-07-20 11:08:42 -0500

    Built the object constructor, pushing up pre-testing in console.

---
+++ trees.js
@@ -0,0 +1,6 @@
+var Tree = function(name, "leaf shape", type, "average height") {
+  this.name = name,
+  this["leaf shape"] = "leaf shape",
+  this.type = type,
+  this["average height"] = "average height"
+}
