ed5141642fb697b4925f8b5a6107babf4af15eaf
Ryan Tanay <ryantanay@newevolution.org>
2015-07-20 11:24:26 -0500

    Added variables with constructor function calls to create the four tree objects.

--- trees.js
+++ trees.js
@@ -5,3 +5,8 @@ var Tree = function(name, leaf_shape, type, average_height) {
   this["average height"] = average_height
 }
 
+var birch = new Tree("Birch", "Serrate", "Deciduous", 50);
+var larch = new Tree("Larch", "Needle", "Deciduous", 140);
+var ponderosa = new Tree("Ponderosa Pine", "Needle", "Evergreen", 60);
+var oak = new Tree("Oak", "Lobed", "Deciduous", 200);
+
