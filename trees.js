92cb873b505e0058581c5ae3a449d4e4175249a6
Ryan Tanay <ryantanay@newevolution.org>
2015-07-20 11:33:13 -0500

    Added tree types to an array, then looped through the array with JSON.stringify to dump the values for each tree into the page.

--- trees.js
+++ trees.js
@@ -1,3 +1,6 @@
+var treeHolder = document.getElementById("tree-holder");
+var treeHolderContent = "";
+
 var Tree = function(name, leaf_shape, type, average_height) {
   this.name = name,
   this["leaf shape"] = leaf_shape,
@@ -10,3 +13,10 @@ var larch = new Tree("Larch", "Needle", "Deciduous", 140);
 var ponderosa = new Tree("Ponderosa Pine", "Needle", "Evergreen", 60);
 var oak = new Tree("Oak", "Lobed", "Deciduous", 200);
 
+treeArray = [birch, larch, ponderosa, oak];
+
+for(var i=0; i < treeArray.length; i++) {
+  treeHolderContent += "<p>" + JSON.stringify(treeArray[i]) + "</p>";
+}
+
+treeHolder.innerHTML = treeHolderContent;
